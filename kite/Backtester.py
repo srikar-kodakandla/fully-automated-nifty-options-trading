4cbb40e1e29c653b8d8716448f91ae9fe6fe1c1f
srikar kodakandla <kodakandlasrikar99@gmail.com>
2022-07-01 20:18:07 +0530

    Update Backtester.py

--- kite/Backtester.py
+++ kite/Backtester.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 22 20:43:15 2020
-
-@author: harpal
-"""
 
 from whatappAtom import WhatsApp
 from MarketSimulator import TickSimulator
