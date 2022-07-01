69d10d04004906318507d836db95ad3ebd975179
srikar kodakandla <kodakandlasrikar99@gmail.com>
2022-07-01 19:37:14 +0530

    Update script.py

--- fyers/script.py
+++ fyers/script.py
@@ -5,23 +5,34 @@ def name_to_script(name):
         import pandas as pd
         import re
         try:
-            a = pd.read_csv('NSE_CM.csv.1')
+            a = pd.read_csv('NSE_CM.csv')
+            #print("current working directory......",os.getcwd())
+           # print(a)
         except:
-            os.system('wget https://public.fyers.in/sym_details/NSE_CM.csv')
+            os.system('curl -0 https://public.fyers.in/sym_details/NSE_CM.csv')
+            #print("exception 1......................")
         finally:
             try:
-                a = pd.read_csv('NSE_CM.csv.1')
+                a = pd.read_csv('NSE_CM.csv')
+                #print("finally1....")
             except:
-                os.system('wget https://public.fyers.in/sym_details/NSE_CM.csv')
-                a = pd.read_csv('NSE_CM.csv.1')
+                os.system('curl -0 https://public.fyers.in/sym_details/NSE_CM.csv')
+                a = pd.read_csv('NSE_CM.csv')
+                
         try:
-            dd=sorted(a[a['NSE:AMARAJABAT-EQ'].str.contains(name, flags=re.IGNORECASE)]['NSE:AMARAJABAT-EQ'].to_numpy())[0]
+            #print("before dd values......")
+            dd=sorted(a[a['NSE:ABAN-EQ'].str.contains(name, flags=re.IGNORECASE)]['NSE:AMARAJABAT-EQ'].to_numpy())[0]
+           
         except:
             try:
-                dd=sorted(a[a['AMARA RAJA BATTERIES LTD.'].str.contains(name, flags=re.IGNORECASE)]['NSE:AMARAJABAT-EQ'].to_numpy())[0]
+                #print("dd2 values..")
+                dd=sorted(a[a['ABAN OFFSHORE LTD.'].str.contains(name, flags=re.IGNORECASE)]['NSE:ABAN-EQ'].to_numpy())[0]
+                
             except:
-                vv = a[a['INE885A01032'].notna()]
-                dd = vv[vv['INE885A01032'].str.contains(name, flags=re.IGNORECASE)]['NSE:AMARAJABAT-EQ'].to_numpy()[0]
+                #print("dd3 values....vv values")
+                vv = a[a['INE421A01028'].notna()]
+                dd = vv[vv['INE421A01028'].str.contains(name, flags=re.IGNORECASE)]['NSE:ABAN-EQ'].to_numpy()[0]
+                
     except Exception as error :
         m.message(f'Either the stock name is wrong or error while downloading data from fyers website \n{error}')
     try:
